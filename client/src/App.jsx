87c16df0321b0c411d6a0513ec6e211cfb1a50da
Pirathaab <sritharan.pirathaab@gmail.com>
2017-03-30 19:04:37 +0200

    App

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from './common/Header.jsx';
+import Footer from './common/Footer.jsx';
+
+export default class App extends Component {
+	
+	render() {
+		
+		return(
+			<div>
+				<Header />
+				<Footer /> 
+			</div>
+		) 
+	}
+
+} 
