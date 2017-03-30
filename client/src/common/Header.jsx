3791f17e832150c08f1da7e02dfe00116b0f9e13
Pirathaab <sritharan.pirathaab@gmail.com>
2017-03-30 19:09:32 +0200

    Header

---
+++ client/src/common/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import '../static/css/header.scss';
+
+export default class Header extends Component {
+
+    render() {
+
+        return (
+          <div className="header">
+            <div>
+              Welcome to THE Blog ;)
+            </div>
+          </div>
+
+        );
+    }
+}
