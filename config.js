92b581ab9b45600fedf24d818ef92b24599b5c8d
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-01 17:21:46 +0200

    db config

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv').config();
+
 module.exports = {
   "port": process.env.PORT || 8000,	
-  "dbUrl": `mongodb://localhost:27017/Blog`
+  "dbUrl": `mongodb://${process.env.USER_DB}:${process.env.PASSWORD_DB}@ds147480.mlab.com:47480/blog`,
+  "jwtSecret": "a secret phrase!!"
 }
