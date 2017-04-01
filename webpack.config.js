6faed8055836d6ee4005fa0d01f9281882252b62
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-01 16:38:45 +0200

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
 	// entry file for your bundle
-	entry: path.join(__dirname, 'client/src/Index.jsx'),
+	entry: path.join(__dirname, 'client/src/App.jsx'),
 
 	// result of the bundle
 	output: {
