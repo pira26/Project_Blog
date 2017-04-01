d5472c2ef65a9aa1f683dfa831cebc4cefd6c041
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-01 18:07:53 +0200

    db config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 
 module.exports = {
   "port": process.env.PORT || 8000,	
-  "dbUrl": `mongodb://${process.env.USER_DB}:${process.env.PASSWORD_DB}@ds147480.mlab.com:47480/blog`,
+  "dbUrl": `mongodb://admin:admin2017@ds147480.mlab.com:47480/blog`,
   "jwtSecret": "a secret phrase!!"
 }
