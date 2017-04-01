548fabb2364b09a4de3e8f6d545135aac26bfc3a
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-01 17:27:13 +0200

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { browserHistory, Router } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
 
 import routes from './routes.js';
 
