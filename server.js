a15f81b75cc71f61a056c22aed6f730095484df9
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-01 17:41:56 +0200

    config server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const morgan = require('morgan');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const config = require('./config.js');
 
