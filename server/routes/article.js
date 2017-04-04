73ea3083005537f2ce974d6970c311ddc6eeb4ad
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-04 18:13:32 +0200

    Article route

---
+++ server/routes/article.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const router = express.Router();
+const Article = require('../models/article.js');
+
+router.route('/articles')
+	.get((req, res) => {
+		Article.find((err, artiles) => {
+			if(err){
+				res.send(err);
+			}
+			res.send(artiles);
+		})
+	})
+	.post((req,res) => {
+		const article = new Article();
+		article.userName = req.body.userName;
+		article.comment = req.body.comment;
+		article.date = Date.now();
+		article.save((err) => {
+			if(err){
+				res.send(err);
+			}
+			res.send({message: 'article have been sended'});
+		});
+	});
+
+router.route('/articles/:article_id')
+	.get((req, res) => {
+		Article.findOne({_id: req.params.article_id}, (err, articles) => {
+			if(err){
+				res.send(err);
+			}
+			res.send(articles)
+		})
+	})
+	.put((req, res) => {
+		Article.findOne({_id: req.params.article_id}, (err, articles) => {
+			article.userName = req.body.userName;
+			article.comment = req.body.comment;
+			article.date = Date.now();
+			article.save((err) => {
+				if(err){
+					res.send(err);
+				}
+				res.send({message: 'article have been updated'});
+			});
+		});
+	})
+	.delete((req, res) => {
+		Article.remove({_id: req.params.article_id}, (err) => {
+			if(err){
+				res.send(err);
+			}
+			res.send({message: 'article has been deleted'});
+		});
+		res.redirect('/articles');
+	});
+
+module.exports = router;
