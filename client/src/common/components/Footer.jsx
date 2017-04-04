b8b155c122f9857f3cfba8dacaf91b1194828804
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-04 18:13:32 +0200

    Footer

---
+++ client/src/common/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import '../static/css/footer.scss';
+
+export default class Footer extends Component {
+
+    render() {
+
+        return (
+          <div className="footer">
+            <div>
+              footer
+            </div>
+          </div>
+
+        );
+    }
+}
