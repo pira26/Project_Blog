5253e9231e3052d05ed9bcd3d33a856f137af4b7
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-04 18:13:32 +0200

    Header

---
+++ client/src/common/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import '../../static/css/header.scss';
+
+export default class Header extends Component {
+
+    render() {
+
+        return (
+        	<div className="header">
+            	<svg viewBox="0 0 100 100" preserveAspectRatio="none">
+    				<polygon fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+  				</svg>
+          	</div>
+
+        );
+    }
+}
