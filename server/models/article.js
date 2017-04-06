0b1641220f6695464fc6a5ef551f903c79d5a039
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-06 15:28:10 +0200

    article schema

--- server/models/article.js
+++ server/models/article.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
 	userName: String,
-	titles: {type: String, required: false},
-	posts: {type: String, required: false},
-  	comments: {type: String, required: false},
-	like: Number
+	titles: String,
+	posts: String,
+  	comments: String,
+	like: Number,
+	picture_url: String
 });
 
 module.exports = mongoose.model('Article', ArticleSchema);
