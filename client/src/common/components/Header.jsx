e25e6647a9cf33684729e12cef1f7145d69ea061
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-06 10:44:01 +0200

    Header

--- client/src/common/components/Header.jsx
+++ client/src/common/components/Header.jsx
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 
 import '../../static/css/header.scss';
 
-export default class Header extends Component {
+const Header = () => (
+	
+	<div className="header">
+    	<svg viewBox="0 0 100 100">
+			<polygon fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+		</svg>
+    </div>
+)
 
-    render() {
-
-        return (
-        	<div className="header">
-            	<svg viewBox="0 0 100 100">
-    				<polygon fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
-  				</svg>
-          	</div>
-
-        );
-    }
-}
+export default Header;
