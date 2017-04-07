4e62fc094e5d196a5724243a20c4984c90eb215e
Pirathaab <sritharan.pirathaab@gmail.com>
2017-04-07 15:13:16 +0200

    Signin Login

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,17 +1,17 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 
-import Auth from '../modules/Auth'; 
+import Auth from '../modules/Auth';
+import Nav from './Nav.jsx';
 
 const Base = ({ children }) => (
-  <div className="col-xs-12
-                  col-sm-12
-                  col-md-12
-                  col-lg-12">
+  <div>
     <div className="top-bar">
       <div className="top-bar-left">
         <IndexLink to="/blog">Blog</IndexLink>
       </div>
+
+      <Nav /> 
 
       {Auth.isUserAuthenticated() ? (
         <div className="top-bar-right">
